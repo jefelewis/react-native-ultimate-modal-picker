a4b75be700aa7493f56cd838d40b35a661dc60e4
Jeff Lewis <jeffylew77@gmail.com>
2021-05-11 12:07:19 -0700

    [Dev] Updating ios files

--- ios/reactNativeUltimateModalPickerTests/reactNativeUltimateModalPickerTests.m
+++ ios/reactNativeUltimateModalPickerTests/reactNativeUltimateModalPickerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeUltimateModalPickerTests : XCTestCase
+@interface ReactNativeUltimateModalPickerTests : XCTestCase
 
 @end
 
-@implementation reactNativeUltimateModalPickerTests
+@implementation ReactNativeUltimateModalPickerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
