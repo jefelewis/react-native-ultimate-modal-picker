50dbcdbacc539b1df93ac122e18c791d1bac6f96
Jeff Lewis <jeffylew77@gmail.com>
2021-05-11 21:20:42 -0700

    [Dev] Adding style types

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -4,30 +4,26 @@ type FontWeight = 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '6
 // TypeScript Type: Text Transform
 type TextTransform = 'uppercase' | 'lowercase' | 'capitalize';
 
+// TypeScript Type: Container
+export interface Container {
+  backgroundColor?: string,
+  borderColor?: string,
+  borderBottomWidth?: number,
+};
+
+// TypeScript Type: Label Text
+export interface LabelText {
+  fontFamily?: string,
+  fontSize?: number,
+  fontWeight?: FontWeight,
+  textTransform?: TextTransform,
+  color?: string,
+};
+
 // TypeScript Type: Date Time Picker Styles
 export interface DateTimePickerStyles {
-  containerLight: {
-    backgroundColor?: string,
-    borderColor?: string,
-    borderBottomWidth?: number,
-  },
-  containerDark: {
-    backgroundColor?: string,
-    borderColor?: string,
-    borderBottomWidth?: number,
-  },
-  labelTextLight: {
-    fontFamily?: string,
-    fontSize?: number,
-    fontWeight?: FontWeight,
-    textTransform?: TextTransform,
-    color?: string,
-  },
-  labelTextDark: {
-    fontFamily?: string,
-    fontSize?: number,
-    fontWeight?: FontWeight,
-    textTransform?: TextTransform,
-    color?: string,
-  },
+  containerLight: Container,
+  containerDark: Container,
+  labelTextLight: LabelText,
+  labelTextDark: LabelText,
 };
