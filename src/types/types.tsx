94f5a234bf443aec5e52e80ecbfe73e1679b42cd
Jeff Lewis <jeffylew77@gmail.com>
2021-05-12 12:15:01 -0700

    [Dev] Adding custom style props

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -5,28 +5,110 @@ type FontWeight = 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '6
 // TypeScript Type: Text Transform
 type TextTransform = 'uppercase' | 'lowercase' | 'capitalize';
 
-// TypeScript Type: Container
-export interface Container {
-  backgroundColor?: string,
-  borderColor?: string,
-  borderBottomWidth?: number,
+// TypeScript Type: Container Style
+export interface ContainerStyle {
+  containerLight: {
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+  containerDark: {
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
 };
 
-// TypeScript Type: Label Text
-export interface LabelText {
-  fontFamily?: string,
-  fontSize?: number,
-  fontWeight?: FontWeight,
-  textTransform?: TextTransform,
-  color?: string,
+// TypeScript Type: Label Text Style
+export interface LabelTextStyle {
+  labelTextLight: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+  labelTextDark: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
 };
 
-// TypeScript Type: Date Time Picker Styles
-export interface DateTimePickerStyles {
-  containerLight: Container,
-  containerDark: Container,
-  labelTextLight: LabelText,
-  labelTextDark: LabelText,
+// TypeScript Type: Field Text Style
+export interface FieldTextStyle {
+  fieldTextLight: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+  fieldTextDark: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+};
+
+// TypeScript Type: Cancel Text Style
+export interface CancelTextStyle {
+  cancelTextLight: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+  cancelTextDark: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+};
+
+// TypeScript Type: Modal Header Container Style
+export interface ModalHeaderContainerStyle {
+  modalHeaderContainerLight: {
+    height?: string,
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+  modalHeaderContainerDark: {
+    height?: string,
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+};
+
+// TypeScript Type: Modal Content Container Style
+export interface ModalContentContainerStyle {
+  modalContentContainerLight: {
+    height?: string,
+    backgroundColor?: string,
+  },
+  modalContentContainerDark: {
+    height?: string,
+    backgroundColor?: string,
+  },
+};
+
+// TypeScript Type: Picker Item Text Style
+export interface PickerItemTextStyle {
+  pickerItemTextLight: {
+    color?: string,
+  },
+  pickerItemTextDark: {
+    color?: string,
+  },
 };
 
 
