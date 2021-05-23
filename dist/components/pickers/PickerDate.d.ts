ce2afaa8b7ae14b33bcfe4310afccb3a4d4dd6aa
Jeff Lewis <jeffylew77@gmail.com>
2021-05-22 21:27:57 -0700

    [Dev] Updating build

---
+++ dist/components/pickers/PickerDate.d.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ContainerStyle, LabelTextStyle } from '../../types/types';
+interface Props {
+    mode: 'calendar' | 'spinner' | 'default';
+    onChange: (date: Date) => void;
+    title?: string;
+    darkMode?: boolean;
+    customStyleContainer?: ContainerStyle;
+    customStyleLabelText?: LabelTextStyle;
+}
+declare const PickerDate: React.FC<Props>;
+export default PickerDate;
---
+++ dist/components/pickers/PickerDateRange.d.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ContainerStyle, LabelTextStyle, DividerStyle, TitleTextStyle } from '../../types/types';
+interface Props {
+    mode: 'calendar' | 'spinner' | 'default';
+    onFromChange: (newDate: Date) => void;
+    onToChange: (newDate: Date) => void;
+    title?: string;
+    darkMode?: boolean;
+    customStyleContainer?: ContainerStyle;
+    customStyleTitleText?: TitleTextStyle;
+    customStyleLabelText?: LabelTextStyle;
+    customStyleDivider?: DividerStyle;
+}
+declare const DateRangePicker: React.FC<Props>;
+export default DateRangePicker;
