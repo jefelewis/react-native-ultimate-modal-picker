5f9293e8f83ee932f7f60955c51e21cbbdb90db5
Jeff Lewis <jeffylew77@gmail.com>
2021-05-22 18:00:24 -0700

    [Fix] Removing files

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import PickerTime from "./components/pickers/PickerTime";
-import PickerDate from "./components/pickers/PickerDate";
-import PickerDateTime from "./components/pickers/PickerDateTime";
-import PickerDateRange from "./components/pickers/PickerDateRange";
-import DropdownList from "./components/dropdowns/DropdownList";
-import DropdownMeasurements from "./components/dropdowns/DropdownMeasurements";
-import DropdownNumber from "./components/dropdowns/DropdownNumber";
-import DropdownState from "./components/dropdowns/DropdownState";
-export { PickerTime, PickerDate, PickerDateTime, PickerDateRange, DropdownList, DropdownMeasurements, DropdownNumber, DropdownState };
